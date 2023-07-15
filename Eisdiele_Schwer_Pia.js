a843375a911b09a6b74a4cff51b0883cbda2797f
Schwer5 <115712200+Schwer5@users.noreply.github.com>
2023-07-15 17:11:36 +0200

    startoberfläche erstellen

---
+++ Eisdiele_Schwer_Pia.js
@@ -0,0 +1,16 @@
+"use strict";
+// Name:<Pia Schwer>
+// Matrikel: <272266>
+// Datum: <15.07.23>
+// Zusammenarbeit mit Theresa Hauser
+// Quellen: Stack Overflow, Developer Mozilla,Github Jirka, Vorherige Aufgabe(n) aus EIA1
+var Eisdiele;
+(function (Eisdiele) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        setInterval(update, 40);
+    }
+    function update() {
+    }
+})(Eisdiele || (Eisdiele = {}));
+//# sourceMappingURL=Eisdiele_Schwer_Pia.js.map
---
+++ Eisdiele_Schwer_Pia.ts
@@ -0,0 +1,19 @@
+// Name:<Pia Schwer>
+// Matrikel: <272266>
+// Datum: <15.07.23>
+// Zusammenarbeit mit Theresa Hauser
+// Quellen: Stack Overflow, Developer Mozilla,Github Jirka, Vorherige Aufgabe(n) aus EIA1
+
+namespace Eisdiele {
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
+        
+        setInterval(update, 40);
+    }
+
+    function update(): void {
+        
+    }
+}
+
